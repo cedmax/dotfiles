8ca8b7bfc749bdfa27d5385a2abcb4b64951d68e
Marco Cedaro <cedmax@users.noreply.github.com>
2017-03-08 19:33:41 +0000

    added npmundler and fixed order of execution

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 
 brew update
 brew doctor
-
-homeshick clone cedmax/dotfiles
-homeshick link dotfiles
-source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-
 brew tap Homebrew/bundle
 brew bundle --file="$HOME/Brewfile"
 
-source "$HOME/.homesick/repos/dotfiles/npm"
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+homeshick clone cedmax/dotfiles
+homeshick link dotfiles
+
+npm install npmundler -g
+npmundler install -g "$HOME/NPMfile"
 
 echo "Remember to install ExpressVPN, Sound Control and VSCode Settings Sync plugin"
