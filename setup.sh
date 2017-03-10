e548abac8d02bd594d3fb42ff1113df086089adf
Marco Cedaro <cedmax@users.noreply.github.com>
2017-03-10 11:48:39 +0000

    removed unneeded (and wrong) shell change

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-chsh -s /usr/local/bin/zsh
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
