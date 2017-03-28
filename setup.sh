739a6697b45e138779506ae41af933c4c36a5615
Marco Cedaro <cedmax@users.noreply.github.com>
2017-03-28 14:17:09 +0100

    better feedback

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+bold=$(tput bold)
+endbold=$(tput sgr0)
+
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -14,9 +17,16 @@ homeshick link dotfiles
 npm install npmundler -g
 npmundler install -g "$HOME/NPMfile"
 
-echo "Please input your Apple Store email: "
+echo "
+${bold}Please input your Apple Store email:${endbold}"
 read apple_store_email
 mas signin --dialog "$apple_store_email"
 source $HOME/Masfile
 
-echo "Remember to install ExpressVPN, Sound Control, Numi and VSCode Settings Sync plugin"
+echo "
+${bold}Automated setup done${endbold}
+Please remember to install also:
+${bold}ExpressVPN${endbold} - https://www.expressvpn.com/vpn-software/vpn-mac
+${bold}Sound Control${endbold} - https://staticz.com/support/soundcontrol/
+${bold}Numi${endbold} - https://numi.io/ 
+${bold}VSCode Settings Sync${endbold} - https://marketplace.visualstudio.com/items?itemName=Shan.code-settings-sync"
